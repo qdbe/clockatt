6dde5847ed8d595fd6314ca7caf8c928cbc88238
godz.qdbe <godz.qdbe@d8bbf296-2aff-11de-ba44-d58fe8a9ce33>
2009-04-18 00:41:50 +0000

    Initial Commit

---
+++ trunk/clockatt/MainForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace clockatt
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.DspTimer = new System.Windows.Forms.Timer(this.components);
+            this.LocateTimer = new System.Windows.Forms.Timer(this.components);
+            this.taskInfoNotify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.ForeColor = System.Drawing.Color.Red;
+            this.timeLabel.Location = new System.Drawing.Point(64, 4);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(109, 12);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "2009/03/21 12:22:45";
+            // 
+            // DspTimer
+            // 
+            this.DspTimer.Tick += new System.EventHandler(this.DspTimer_Tick);
+            // 
+            // LocateTimer
+            // 
+            this.LocateTimer.Enabled = true;
+            this.LocateTimer.Interval = 10;
+            this.LocateTimer.Tick += new System.EventHandler(this.LocateTimer_Tick);
+            // 
+            // taskInfoNotify
+            // 
+            this.taskInfoNotify.Text = "ClockAtt";
+            this.taskInfoNotify.Visible = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(174, 19);
+            this.Controls.Add(this.timeLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.ShowInTaskbar = false;
+            this.Text = "MainForm";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Timer DspTimer;
+        private System.Windows.Forms.Timer LocateTimer;
+        private System.Windows.Forms.NotifyIcon taskInfoNotify;
+    }
+}
---
+++ trunk/clockatt/MainForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Collections;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using W32NativeService;
+using System.Runtime.InteropServices;
+
+
+namespace clockatt
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.SetStyle(
+                ControlStyles.DoubleBuffer |
+                ControlStyles.UserPaint |
+                ControlStyles.AllPaintingInWmPaint,
+                true);
+
+            this.UpdateStyles();
+        }
+
+        private void LocateTimer_Tick(object sender, EventArgs e)
+        {
+            int hwnd =  W32Native.GetForegroundWindow();
+            if (hwnd == (int)this.Handle)
+            {
+                return;
+            }
+
+            W32Native.wWINDOWINFO info = new W32Native.wWINDOWINFO();
+            info.cbSize = (uint)Marshal.SizeOf(info);
+            bool ret = W32Native.GetWindowInfo((IntPtr)hwnd, ref info);
+            if (ret == true)
+            {
+                W32Native.wTITLEBARINFO tbi = new W32Native.wTITLEBARINFO();
+                tbi.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(tbi);
+                bool result = W32Native.GetTitleBarInfo((IntPtr)hwnd, ref tbi); 
+                // Get TitleBar Size
+                int titleHeight = tbi.rcTitleBar.bottom - tbi.rcTitleBar.top;
+                int titleWidth = tbi.rcTitleBar.right - tbi.rcTitleBar.left;
+                ArrayList ar = new ArrayList();
+                ar.Add(W32Native.wTITLEELEMENT.TITLE_CLOSE_BUTTON);
+                ar.Add(W32Native.wTITLEELEMENT.TITLE_HELP_BUTTON);
+                ar.Add(W32Native.wTITLEELEMENT.TITLE_MAX_BUTTON);
+                ar.Add(W32Native.wTITLEELEMENT.TITLE_MIN_BUTTON);
+                int leftLength = 0;
+                for( int i = 0; i < ar.Count; i++ )
+                {
+                    if (
+                        !((tbi.rgstate[(int)ar[i]] & W32Native.STATE_SYSTEM_INVISIBLE) == W32Native.STATE_SYSTEM_INVISIBLE) &&
+                        !((tbi.rgstate[(int)ar[i]] & W32Native.STATE_SYSTEM_OFFSCREEN) == W32Native.STATE_SYSTEM_OFFSCREEN)
+                    )
+                    {
+                        leftLength += titleHeight - 2;
+                    }
+                }
+                this.Location = new Point(info.rcWindow.right - this.Width - leftLength, info.rcWindow.top + 1);
+            }
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.DspTimer.Start();
+            this.LocateTimer.Start();
+            this.taskInfoNotify.Visible = true;
+        }
+
+        private void DspTimer_Tick(object sender, EventArgs e)
+        {
+            this.SetTimeLabel();
+        }
+
+        private void SetTimeLabel()
+        {
+            this.timeLabel.Text = DateTime.Now.ToString();
+            this.taskInfoNotify.BalloonTipText = this.timeLabel.Text;
+        }
+    }
+}
