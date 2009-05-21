d9471c237dcffb2105b54760d7bf4c8f4d1551e1
godz.qdbe <godz.qdbe@d8bbf296-2aff-11de-ba44-d58fe8a9ce33>
2009-05-21 15:08:22 +0000

    (ref #2)

--- clockatt/CalenderForm.Designer.cs
+++ clockatt/CalenderForm.Designer.cs
@@ -28,73 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
-            this.lblMonthNow = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button1.Location = new System.Drawing.Point(144, 116);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(0, 0);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // monthCalendar1
-            // 
-            this.monthCalendar1.Location = new System.Drawing.Point(156, 28);
-            this.monthCalendar1.Name = "monthCalendar1";
-            this.monthCalendar1.TabIndex = 3;
-            this.monthCalendar1.TitleBackColor = System.Drawing.SystemColors.GrayText;
-            // 
-            // lblMonthNow
-            // 
-            this.lblMonthNow.AutoSize = true;
-            this.lblMonthNow.Location = new System.Drawing.Point(37, 40);
-            this.lblMonthNow.Name = "lblMonthNow";
-            this.lblMonthNow.Size = new System.Drawing.Size(59, 12);
-            this.lblMonthNow.TabIndex = 4;
-            this.lblMonthNow.Text = "2009年4月";
-            // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(13, 59);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(131, 114);
-            this.panel1.TabIndex = 5;
             // 
             // CalenderForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.button1;
-            this.ClientSize = new System.Drawing.Size(558, 454);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.lblMonthNow);
-            this.Controls.Add(this.monthCalendar1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(165, 179);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "CalenderForm";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "CalenderForm";
-            this.Load += new System.EventHandler(this.CalenderForm_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.CalenderForm_MouseDown);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.MonthCalendar monthCalendar1;
-        private System.Windows.Forms.Label lblMonthNow;
-        private System.Windows.Forms.Panel panel1;
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
--- clockatt/CalenderForm.cs
+++ clockatt/CalenderForm.cs
@@ -5,25 +5,201 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace clockatt
 {
     public partial class CalenderForm : Form
     {
+        private int YearMonthTop = 10;
+        private int YearMonthLeft = 10;
+
+        private int WeekDayTop = 10;
+        private int WeekDayLeft = 10;
+
+
+        private int DayTop = 10;
+        private int DayLeft = 10;
+
+        private int DispYear;
+        private int DispMonth;
+
+        private int CharMargin = 10;
+
+        private int fontSize = 12;
+
         public CalenderForm()
         {
             InitializeComponent();
+            DateTime dt = DateTime.Now;
+            this.DispYear = dt.Year;
+            this.DispMonth = dt.Month;
         }
 
-        private void CalenderForm_Load(object sender, EventArgs e)
+        protected override void OnPaint(PaintEventArgs e)
         {
-            this.monthCalendar1.TodayDate = DateTime.Now;
+            this.Paints(e.ClipRectangle, e.Graphics);
         }
 
-        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        protected virtual void Paints(Rectangle clip, Graphics g)
         {
+            g.SetClip(clip);
+            // 年＋月の描画
+            PaintYearMonth(clip, g);
+            // 曜日の描画
+            PaintWeekDay(clip, g);
+            // 日付の描画
+            PaintDay(clip, g);
+        }
+
+        protected virtual void PaintYearMonth(Rectangle clip, Graphics g)
+        {
+            int x = YearMonthLeft;
+            int y = YearMonthTop;
+
+            Font yearMonthFont = new Font("ＭＳ ゴシック", fontSize);
+            this.CharMargin = yearMonthFont.Height;
+            System.Drawing.Brush yearMonthBrush = new System.Drawing.SolidBrush(Color.Black);
+
+            this.WeekDayTop = YearMonthTop + yearMonthFont.Height + 5;
+
+            string dispString = string.Format("{0}年 {1}月",
+                this.DispYear,
+                this.DispMonth
+                );
+
+            g.DrawString(dispString, yearMonthFont, yearMonthBrush, x, y);
 
         }
 
+        protected virtual void PaintWeekDay(Rectangle clip, Graphics g)
+        {
+            int x = WeekDayLeft;
+            int y = WeekDayTop;
+            Font weekDayFont = new Font("ＭＳ ゴシック", fontSize);
+            this.CharMargin = weekDayFont.Height + 5;
+            System.Drawing.Brush weekDayBrush = new System.Drawing.SolidBrush(Color.Black);
+            System.Drawing.Brush weekDayBrushSun = new System.Drawing.SolidBrush(Color.Red);
+            System.Drawing.Brush weekDayBrushSat = new System.Drawing.SolidBrush(Color.Blue);
+
+            this.DayTop = WeekDayTop + weekDayFont.Height + 0;
+
+            object [][] strWeekDay = new object[][]{
+                        new object[]{ "日", weekDayBrushSun },
+                        new object[]{ "月", weekDayBrush },
+                        new object[]{ "火", weekDayBrush },
+                        new object[]{ "水", weekDayBrush },
+                        new object[]{ "木", weekDayBrush },
+                        new object[]{ "金", weekDayBrush },
+                        new object[]{ "土", weekDayBrushSat }
+            };
+
+            Brush b = weekDayBrush;
+            for (int i = 0; i < strWeekDay.Length; i++)
+            {
+                g.DrawString(strWeekDay[i][0].ToString(), weekDayFont, (Brush)strWeekDay[i][1], x, y);
+                x += this.CharMargin;
+            }
+        }
+
+        protected virtual void PaintDay(Rectangle clip, Graphics g)
+        {
+            int x = DayLeft;
+            int y = DayTop;
+            int addHeight = 0;
+            Font dayFont = new Font("ＭＳ ゴシック", fontSize);
+            this.CharMargin = dayFont.Height + 5;
+            System.Drawing.Brush dayBrush = new System.Drawing.SolidBrush(Color.Black);
+            System.Drawing.Brush dayBrushHolyDay = new System.Drawing.SolidBrush(Color.Red);
+            System.Drawing.Brush dayBrushSun = new System.Drawing.SolidBrush(Color.Red);
+            System.Drawing.Brush dayBrushSat = new System.Drawing.SolidBrush(Color.Blue);
+            System.Drawing.Brush dayBrushToDay = new System.Drawing.SolidBrush(Color.Aqua);
+
+            addHeight = dayFont.Height + 0;
+
+
+            DateTime dt = new DateTime(this.DispYear, this.DispMonth, 1);
+
+            if (dt.DayOfWeek != DayOfWeek.Sunday)
+            {
+                if (dt.DayOfWeek == DayOfWeek.Monday)
+                {
+                    x += this.CharMargin;
+                }
+                if (dt.DayOfWeek == DayOfWeek.Tuesday)
+                {
+                    x += this.CharMargin * 2;
+                }
+                if (dt.DayOfWeek == DayOfWeek.Wednesday)
+                {
+                    x += this.CharMargin * 3;
+                }
+                if (dt.DayOfWeek == DayOfWeek.Thursday)
+                {
+                    x += this.CharMargin * 4;
+                }
+                if (dt.DayOfWeek == DayOfWeek.Friday)
+                {
+                    x += this.CharMargin * 5;
+                }
+                if (dt.DayOfWeek == DayOfWeek.Saturday)
+                {
+                    x += this.CharMargin * 6;
+                }
+            }
+
+
+            Brush b = dayBrush;
+            for (int i = 0; ; i++)
+            {
+                if (dt.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    x = DayLeft;
+                    if (i != 0)
+                    {
+                        y += addHeight;
+                    }
+                    b = dayBrushSun;
+                }
+                else if (dt.DayOfWeek == DayOfWeek.Saturday)
+                {
+                    b = dayBrushSat;
+                }
+                else
+                {
+                    b = dayBrush;
+                }
+                string dispstr = string.Format("{0, 2}", dt.Day);
+                g.DrawString(dispstr, dayFont, b, x, y);
+
+                x += this.CharMargin;
+
+                dt = dt.AddDays(1);
+                if (dt.Month != this.DispMonth)
+                {
+                    break;
+                }
+            }
+        }
+
+        private void CalenderForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            int limits = this.Width / 2;
+
+            DateTime dt = new DateTime(this.DispYear, this.DispMonth, 1);
+            if (e.X < limits)
+            {
+                dt = dt.AddMonths(-1);
+            }
+            else
+            {
+                dt = dt.AddMonths(1);
+            }
+            this.DispMonth = dt.Month;
+            this.DispYear = dt.Year;
+
+            this.Invalidate();
+
+        }
     }
 }
