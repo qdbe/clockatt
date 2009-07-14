0e98d69694fa2e3968dbcc9c15199c0f1d00062d
godz.qdbe <godz.qdbe@d8bbf296-2aff-11de-ba44-d58fe8a9ce33>
2009-07-14 03:58:06 +0000

    (ref #3)

---
+++ trunk/clockatt/FormControls/FontSelector.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace clockatt.FormControls
+{
+    public partial class FontSelector : UserControl
+    {
+        private Font pSelectedFont;
+
+        public Font SelectedFont
+        {
+            get { return pSelectedFont; }
+            set { 
+                pSelectedFont = value;
+                this.txtFontName.Text = pSelectedFont.Name;
+                this.toolTip1.SetToolTip(this.txtFontName, this.txtFontName.Text);
+            }
+        }
+
+        private Control pSampleControl;
+        public Control SampleControl
+        {
+            get { return pSampleControl; }
+        }
+
+        private string pSampleProperty;
+        public string SampleProperty
+        {
+            get { return pSampleProperty; }
+        }
+
+
+        public FontSelector()
+        {
+            InitializeComponent();
+            this.SelectedFont = this.Font;
+        }
+
+        private void btnFontSearch_Click(object sender, EventArgs e)
+        {
+            this.fontSelectDialog.Font = this.SelectedFont;
+            if (this.fontSelectDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                this.SelectedFont = this.fontSelectDialog.Font;
+            }
+        }
+
+        public void SetSampleControl(Control target, string targetProperty)
+        {
+            this.pSampleControl = target;
+            this.pSampleProperty = targetProperty;
+        }
+    }
+}
