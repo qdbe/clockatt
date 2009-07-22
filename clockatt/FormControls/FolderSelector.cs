ac704785f2636659c63ce38408244903f2d51065
godz.qdbe <godz.qdbe@d8bbf296-2aff-11de-ba44-d58fe8a9ce33>
2009-07-22 15:07:48 +0000

    (ref #13)

---
+++ clockatt/FormControls/FolderSelector.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace clockatt.FormControls
+{
+    public partial class FolderSelector : ConfigSelectorBase
+    {
+        public FolderSelector()
+        {
+            InitializeComponent();
+            this.SetValue(string.Empty);
+        }
+
+        public override void SetValue(object value)
+        {
+            if (!(value is string))
+            {
+                throw new ArgumentException(value.GetType().Name + "は引数にセットできません");
+            }
+            pSelectedValue = value;
+            this.txtFolderName.Text = (string)value;
+            this.toolTip1.SetToolTip(this.txtFolderName, this.txtFolderName.Text);
+            this.SetSampleProperty(this.pSelectedValue);
+        }
+
+        private void btnFolderSearch_Click(object sender, EventArgs e)
+        {
+            this.folderSelectDlg.SelectedPath = this.txtFolderName.Text;
+            this.folderSelectDlg.ShowNewFolderButton = true;
+            if (this.folderSelectDlg.ShowDialog(this) == DialogResult.OK)
+            {
+                this.txtFolderName.Text = this.folderSelectDlg.SelectedPath;
+                SetValue(this.folderSelectDlg.SelectedPath);
+            }
+        }
+
+        protected override Type GetSamplePropertyType()
+        {
+            return typeof(string);
+        }
+
+        private void FolderSelector_Enter(object sender, EventArgs e)
+        {
+            this.errorProvider1.SetError(this, "");
+        }
+
+        private void FolderSelector_Validating(object sender, CancelEventArgs e)
+        {
+            if (this.Enabled == true)
+            {
+                if (this.txtFolderName.Text == string.Empty)
+                {
+                    this.errorProvider1.SetError(this, "フォルダを指定して下さい");
+                    e.Cancel = true;
+                    return;
+                }
+                DirectoryInfo di = new DirectoryInfo(this.txtFolderName.Text);
+                if (di == null)
+                {
+                    this.errorProvider1.SetError(this, "指定されたフォルダは存在しません。正しいフォルダを指定してください");
+                    e.Cancel = true;
+                }
+                else if (di.Exists == false)
+                {
+                    this.errorProvider1.SetError(this, "指定されたフォルダは存在しません。正しいフォルダを指定してください");
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void FolderSelector_Validated(object sender, EventArgs e)
+        {
+            this.pSelectedValue = this.txtFolderName.Text;
+        }
+
+    }
+}
---
+++ clockatt/TitileHistoryLogger.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace clockatt
+{
+    public class TitileHistoryLogger : IDisposable
+    {
+        private int LogRetainDay;
+        private string LogDir;
+        private FileInfo LogFile;
+        private const string LogFileNameFormat = "yyyyMMdd";
+        private StreamWriter logStreamWriter;
+        private string preOutput = string.Empty;
+        private const string LogFileFormat = "{0}\t{1}\t{2}";
+
+        public TitileHistoryLogger()
+        {
+            this.LogFile = null;
+        }
+
+        public void LogOutput(int logRetainDay, string logDir, string titleText)
+        {
+            if (preOutput == titleText)
+            {
+                return;
+            }
+            preOutput = titleText;
+
+            this.LogRetainDay = logRetainDay;
+            this.LogDir = logDir;
+            if (this.LogDir.EndsWith(@"\") == false)
+            {
+                this.LogDir = this.LogDir + "\\";
+            }
+            DateTime current = DateTime.Now;
+            FileInfo fi = new FileInfo(this.LogDir +
+                current.ToString(LogFileNameFormat) +
+                ".log");
+            OpenLogFile(fi);
+            DeleteOldLogIfNeed(current);
+
+            string writeLog = string.Format(
+                LogFileFormat,
+                current.ToShortDateString(),
+                current.ToShortTimeString(),
+                titleText);
+
+            this.logStreamWriter.WriteLine(writeLog);
+        }
+
+        private void OpenLogFile(FileInfo newfile)
+        {
+            if (this.LogFile != null && 
+                this.LogFile.FullName == newfile.FullName)
+            {
+                return;
+            }
+            this.LogFile = newfile;
+            if (this.logStreamWriter != null)
+            {
+                this.logStreamWriter.Close();
+            }
+            this.logStreamWriter = this.LogFile.CreateText();
+        }
+
+        private void DeleteOldLogIfNeed(DateTime current)
+        {
+            DirectoryInfo di = new DirectoryInfo(this.LogDir);
+            FileInfo []files = di.GetFiles("*.log");
+            DateTime deleteDate = new DateTime(current.Ticks);
+            deleteDate.AddDays(-this.LogRetainDay);
+            string currentName = current.ToString(LogFileNameFormat);
+            
+            for (int i = 0; i < files.Length; i++)
+            {
+                string basename = files[i].Name.Replace(files[i].Extension, "");
+                if (currentName.CompareTo(basename) > 0)
+                {
+                    files[i].Delete();
+                }
+            }
+        }
+
+
+        #region IDisposable メンバ
+
+        public void Dispose()
+        {
+            if (logStreamWriter != null)
+            {
+                logStreamWriter.Close();
+                logStreamWriter.Dispose();
+                logStreamWriter = null;
+            }
+        }
+
+        #endregion
+    }
+}
