070603c48fe7b3d36d057afb9a279426314ae4c3
godz.qdbe <godz.qdbe@d8bbf296-2aff-11de-ba44-d58fe8a9ce33>
2009-09-09 13:05:40 +0000

    issue #14: リファクタリング(Release0.1)
    (ref #14)

--- clockatt/MainForm.Designer.cs
+++ clockatt/MainForm.Designer.cs
@@ -38,6 +38,7 @@
             this.miShowAbout = new System.Windows.Forms.ToolStripMenuItem();
             this.miShowHelp = new System.Windows.Forms.ToolStripMenuItem();
             this.miQuit = new System.Windows.Forms.ToolStripMenuItem();
+            this.miShowHide = new System.Windows.Forms.ToolStripMenuItem();
             this.RightClickMenu.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -60,31 +61,32 @@
             // 
             this.RightClickMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.miConfigration,
+            this.miShowHide,
             this.miShowAbout,
             this.miShowHelp,
             this.miQuit});
             this.RightClickMenu.Name = "RightClickMenu";
             this.RightClickMenu.ShowImageMargin = false;
-            this.RightClickMenu.Size = new System.Drawing.Size(92, 92);
+            this.RightClickMenu.Size = new System.Drawing.Size(128, 136);
             // 
             // miConfigration
             // 
             this.miConfigration.Name = "miConfigration";
-            this.miConfigration.Size = new System.Drawing.Size(91, 22);
+            this.miConfigration.Size = new System.Drawing.Size(127, 22);
             this.miConfigration.Text = "設定(&C)";
             this.miConfigration.Click += new System.EventHandler(this.miConfigClock_Click);
             // 
             // miShowAbout
             // 
             this.miShowAbout.Name = "miShowAbout";
-            this.miShowAbout.Size = new System.Drawing.Size(91, 22);
+            this.miShowAbout.Size = new System.Drawing.Size(127, 22);
             this.miShowAbout.Text = "About(&A)";
             this.miShowAbout.Click += new System.EventHandler(this.miShowAbout_Click);
             // 
             // miShowHelp
             // 
             this.miShowHelp.Name = "miShowHelp";
-            this.miShowHelp.Size = new System.Drawing.Size(91, 22);
+            this.miShowHelp.Size = new System.Drawing.Size(127, 22);
             this.miShowHelp.Text = "ヘルプ(&H)";
             this.miShowHelp.Click += new System.EventHandler(this.miShowHelp_Click);
             // 
@@ -93,16 +95,23 @@
             this.miQuit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.miQuit.Name = "miQuit";
             this.miQuit.ShortcutKeyDisplayString = "";
-            this.miQuit.Size = new System.Drawing.Size(91, 22);
+            this.miQuit.Size = new System.Drawing.Size(127, 22);
             this.miQuit.Text = "終了(&Q)";
             this.miQuit.Click += new System.EventHandler(this.miQuit_Click);
+            // 
+            // miShowHide
+            // 
+            this.miShowHide.Name = "miShowHide";
+            this.miShowHide.Size = new System.Drawing.Size(127, 22);
+            this.miShowHide.Text = "非表示にする";
+            this.miShowHide.Click += new System.EventHandler(this.miShowHide_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = global::clockatt.Properties.Settings.Default.BackColor;
-            this.ClientSize = new System.Drawing.Size(154, 100);
+            this.ClientSize = new System.Drawing.Size(154, 34);
             this.ContextMenuStrip = this.RightClickMenu;
             this.DataBindings.Add(new System.Windows.Forms.Binding("BackColor", global::clockatt.Properties.Settings.Default, "BackColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
             this.DataBindings.Add(new System.Windows.Forms.Binding("Font", global::clockatt.Properties.Settings.Default, "Font", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
@@ -138,5 +147,6 @@
         private System.Windows.Forms.ToolStripMenuItem miConfigration;
         private System.Windows.Forms.ToolStripMenuItem miShowAbout;
         private System.Windows.Forms.ToolStripMenuItem miShowHelp;
+        private System.Windows.Forms.ToolStripMenuItem miShowHide;
     }
 }
